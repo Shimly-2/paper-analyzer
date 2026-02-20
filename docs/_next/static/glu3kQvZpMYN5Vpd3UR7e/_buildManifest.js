f45de6dc06526ec80e41b36699c838f070cc7c3f
Shimly <shimly@example.com>
2026-02-21 02:27:18 +0800

    feat: move to docs folder

--- docs/_next/static/glu3kQvZpMYN5Vpd3UR7e/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-fc04105c3b1db27d.js"],"/_error":["static/chunks/pages/_error-16075e78949bdb8e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
